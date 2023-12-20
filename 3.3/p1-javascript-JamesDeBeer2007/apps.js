58cb1851dac675aa2bcb9efab19c32b61d0e0e0b
James <143798522+JamesDeBeer2007@users.noreply.github.com>
2023-12-20 10:36:18 +0100

    evyting

---
+++ 3.3/p1-javascript-JamesDeBeer2007/apps.js
@@ -0,0 +1,4 @@
+alert("hello world")
+document.getElementById("title").style.color = "red"
+document.getElementById("paragraaf").style.color = "red"
+document.getElementById("title").textContent = "ljgtchgchuf"
